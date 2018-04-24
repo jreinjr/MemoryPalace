cb182499ca9352e1d3bd15f4c60e93e72578dc45
Jason Reinhardt <jason.w.reinhardt@gmail.com>
2018-04-25 01:49:11 +0200

    180424_03
    Can place two decals from menu but it's buggy as fuck

--- Assets/Scripts/SpawnPoster.cs
+++ Assets/Scripts/SpawnPoster.cs
@@ -8,6 +8,7 @@ public class SpawnPoster : MonoBehaviour {
     public GameObject poster;
     GameObject posterInstance;
     VRTK_Pointer pointer;
+    Texture texture;
 
     private void Start()
     {
@@ -17,9 +18,17 @@ public class SpawnPoster : MonoBehaviour {
     public void Spawn()
     {
         RaycastHit hit = pointer.pointerRenderer.GetDestinationHit();
+        if (pointer.IsStateValid()) { }
         // Offset slightly by normal to avoid coplanar surfaces
         Vector3 instancePos = hit.point + hit.normal * 0.0001f;
         Quaternion instanceRot = Quaternion.LookRotation(hit.normal * -1);
-        Instantiate(poster, instancePos, instanceRot);
+        posterInstance = Instantiate(poster, instancePos, instanceRot);
+        posterInstance.GetComponent<Renderer>().material.mainTexture = texture;
+        
+    }
+
+    public void SetPoster(Texture test)
+    {
+        texture = test;
     }
 }
