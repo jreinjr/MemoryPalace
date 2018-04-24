e510079d444e050737b74e26f4566b37a9b6c9dc
Jason Reinhardt <jason.w.reinhardt@gmail.com>
2018-04-24 20:41:28 +0200

    180424_02
    Can teleport around demo room and spawn posters of Eiffel Tower

---
+++ Assets/Scripts/SpawnPoster.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class SpawnPoster : MonoBehaviour {
+
+    public GameObject poster;
+    GameObject posterInstance;
+    VRTK_Pointer pointer;
+
+    private void Start()
+    {
+        pointer = GetComponent<VRTK_Pointer>();
+    }
+
+    public void Spawn()
+    {
+        RaycastHit hit = pointer.pointerRenderer.GetDestinationHit();
+        // Offset slightly by normal to avoid coplanar surfaces
+        Vector3 instancePos = hit.point + hit.normal * 0.0001f;
+        Quaternion instanceRot = Quaternion.LookRotation(hit.normal * -1);
+        Instantiate(poster, instancePos, instanceRot);
+    }
+}
