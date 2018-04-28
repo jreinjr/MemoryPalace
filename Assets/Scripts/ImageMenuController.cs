fd0b54a1b5df2d0c73f6dbbd761eaa11d8c8fc18
Jason Reinhardt <jason.w.reinhardt@gmail.com>
2018-04-28 22:15:30 +0200

    Revert "Revert "180428_01""
    This reverts commit 8cea5aa0a1f0adcc5f5bac77a6b1dff7e4eb1263.

--- Assets/Scripts/ImageMenuController.cs
+++ Assets/Scripts/ImageMenuController.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEditor;
 
 public class ImageMenuController : MonoBehaviour {
 
     // The button prefab to instantiate
     public GameObject buttonPrefab;
     // Location of the image folder
-    public string filePath = "Assets/Textures/";
+    private string filePath = "Textures";
+    private Object[] sprites;
 
     // Use this for initialization
     void Start () {
@@ -17,6 +19,15 @@ public class ImageMenuController : MonoBehaviour {
 	
 	protected void BuildMenu()
     {
-
+        sprites = Resources.LoadAll(filePath, typeof(Sprite));
+        foreach (var t in sprites)
+        {
+            GameObject buttonInstance = Instantiate(buttonPrefab, transform);
+            ImageButton imageButton = buttonInstance.GetComponent<ImageButton>();
+            imageButton.label = t.name;
+            imageButton.img = t as Sprite;
+        }
+        
     }
+    
 }
--- Assets/VRTK/Scripts/UI/VRTK_UIDraggableItem.cs
+++ Assets/VRTK/Scripts/UI/VRTK_UIDraggableItem.cs
@@ -179,6 +179,7 @@ namespace VRTK
             {
                 dragTransform = eventData.pointerEnter.transform as RectTransform;
             }
+            
 
             Vector3 pointerPosition;
             if (dragTransform != null && RectTransformUtility.ScreenPointToWorldPointInRectangle(dragTransform, eventData.position, eventData.pressEventCamera, out pointerPosition))
@@ -186,6 +187,7 @@ namespace VRTK
                 transform.position = pointerPosition - (transform.forward * forwardOffset);
                 transform.rotation = dragTransform.rotation;
             }
+            
         }
 
         protected virtual void ResetElement()
